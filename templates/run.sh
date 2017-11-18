7adf8021fa7e54f0664e2f368db924de016d085c
amit786 <amitborulkar@gmail.com>
2017-11-17 22:49:35 -0500

    Initial code drop with Cinder and Swift

---
+++ templates/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+openstack overcloud deploy --templates \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/templates/flexpod.yaml \
+-e /home/stack/templates/net-vlans.yaml \
+--control-flavor control --compute-flavor compute  \
+--compute-scale 1 --control-scale 3  \
+--neutron-network-type vlan \
+--neutron-tunnel-type vlan \
+--neutron-bridge-mappings datacentre:br-ex,physnet-tenant:br-tenant,floating:br-floating \
+--neutron-network-vlan-ranges physnet-tenant:650:750,floating:160:160 \
+--ntp-server 10.1.166.410.1.166.410.1.166.410.1.166.4  -t 60
